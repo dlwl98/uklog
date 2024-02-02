7885acc5a9c060d3cc1ed1beb10937860cc10330
dlwl98 <ff981113@hanyang.ac.kr>
2024-02-02 16:16:42 +0900

    feat: 수정 페이지 작성

---
+++ app/posts/[id]/edit/page.tsx
@@ -0,0 +1,49 @@
+import { PostsService } from '@/app/_lib/posts/Posts.service';
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
+async function handleSubmit(formData: FormData) {
+  'use server';
+  const id = formData.get('id')?.toString();
+  const title = formData.get('title')?.toString();
+  const content = formData.get('content')?.toString();
+  if (title && content) {
+    await PostsService.updatePost(id!, { title, content });
+    revalidatePath('/');
+    redirect(`/posts/${id}`);
+  }
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  if (cookies().get('loggedIn')?.value !== 'true') {
+    redirect('/login');
+  }
+  const post = await PostsService.getPostById(params.id);
+  if (!post) {
+    throw new Error(`cannot find post id: ${params.id}`);
+  }
+
+  const { title, content } = post;
+
+  return (
+    <div>
+      <Link href="/">
+        <button>리스트로</button>
+      </Link>
+      <form action={handleSubmit}>
+        <input type="hidden" name="id" defaultValue={params.id} readOnly />
+        <div>
+          <span>title</span>
+          <input type="text" name="title" defaultValue={title} />
+        </div>
+        <div>
+          <span>content</span>
+          <input type="text" name="content" defaultValue={content} />
+        </div>
+        <button type="submit">post!</button>
+      </form>
+    </div>
+  );
+}
