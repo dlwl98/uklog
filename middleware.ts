74ab87c596e17b1c6508c4117b2c96a51b1b29eb
dlwl98 <ff981113@hanyang.ac.kr>
2024-02-06 17:50:15 +0900

    feat: 인증 필요한 페이지 진입 못하게하는 기능 추가

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,34 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 
+function isLoginRequire(pathname: string) {
+  const loginRequirePathMatchers = [/\/write/, /\/posts\/[^\/]+\/edit$/];
+  for (const matcher of loginRequirePathMatchers) {
+    if (pathname.match(matcher)) {
+      return true;
+    }
+  }
+  return false;
+}
+
 export async function middleware(request: NextRequest) {
+  const response = new NextResponse();
   const token = request.cookies.get('token')?.value;
 
-  if (!token) {
-    const response = NextResponse.next();
-    response.cookies.set('loggedIn', 'false');
-    return response;
-  }
-
-  const response = NextResponse.next();
   try {
+    if (!token) {
+      throw new Error('Token not found');
+    }
     await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET!));
     response.cookies
       .set('loggedIn', 'true')
       .set('token', token, { httpOnly: true });
+    return NextResponse.next(response);
   } catch (error) {
     response.cookies.set('loggedIn', 'false');
-  } finally {
-    return response;
+    if (isLoginRequire(request.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+    return NextResponse.next(response);
   }
 }
