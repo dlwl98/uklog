50ce7c46682641980a599e55356f6953f32362d0
dlwl98 <ff981113@hanyang.ac.kr>
2024-02-09 21:42:39 +0900

    design: 게시글 본문 css 수정

--- app/(root)/posts/[id]/Content.tsx
+++ app/(root)/posts/[id]/Content.tsx
@@ -1,11 +1,20 @@
 'use client';
 
 import MDEditor from '@uiw/react-md-editor';
+import stylex from '@stylexjs/stylex';
+import { layout } from '@/app/global.stylex';
 
 export default function Content({ source }: { source: string }) {
   return (
-    <div data-color-mode="light">
+    <div
+      {...stylex.props(layout.default, styles.layout)}
+      data-color-mode="light"
+    >
       <MDEditor.Markdown source={source} />
     </div>
   );
 }
+
+const styles = stylex.create({
+  layout: { padding: '10px' },
+});
