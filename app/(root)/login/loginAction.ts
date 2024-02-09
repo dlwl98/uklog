cd95af5ae806933aa31a56b72590fe823ac49491
dlwl98 <ff981113@hanyang.ac.kr>
2024-02-09 22:59:51 +0900

    refactor: loginAction 가독성 좋게 수정

--- app/(root)/login/loginAction.ts
+++ app/(root)/login/loginAction.ts
@@ -9,23 +9,24 @@ export async function login(
   formData: FormData,
 ) {
   const password = formData.get('password')?.toString();
-  if (password) {
-    if (verifyPassword(password)) {
-      const token = await new SignJWT({ user: 'admin' })
-        .setProtectedHeader({ alg: 'HS256' })
-        .setExpirationTime('72h')
-        .sign(new TextEncoder().encode(process.env.JWT_SECRET!));
-
-      cookies().set('loggedIn', 'true');
-      cookies().set('token', token, {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 3,
-      });
-      return { message: '', success: true };
-    } else {
-      return { message: '비밀번호가 일치하지 않습니다', success: false };
-    }
-  } else {
+  if (!password) {
     return { message: '비밀번호를 입력해주세요', success: false };
   }
+
+  if (!verifyPassword(password)) {
+    return { message: '비밀번호가 일치하지 않습니다', success: false };
+  }
+
+  const token = await new SignJWT({ user: 'admin' })
+    .setProtectedHeader({ alg: 'HS256' })
+    .setExpirationTime('72h')
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET!));
+
+  cookies().set('loggedIn', 'true');
+  cookies().set('token', token, {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 3,
+  });
+
+  return { message: '', success: true };
 }
