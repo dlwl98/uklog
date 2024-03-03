5257cc562a3e5c9ba1ba23b4ca799ed19f445490
dlwl98 <ff981113@hanyang.ac.kr>
2024-03-03 16:41:40 +0900

    fix: ScrollTop 컴포넌트 behavior instant로 변경

--- app/(root)/ScrollTop.tsx
+++ app/(root)/ScrollTop.tsx
@@ -8,7 +8,7 @@ export default function ScrollTop() {
 
   useLayoutEffect(() => {
     if (window.location.hash === '') {
-      window.scrollTo(0, 0);
+      window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
     }
   }, [pathname]);
 
